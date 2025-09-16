77e39baa3983705a4b46a779f504d9f621bc64d4
pushkar_kitawat <pushkarkitawat123456@gmail.com>
2025-09-16 11:37:32 +0530

    fronthandcode

---
+++ src/Admin/bluebusloader.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './loader.css';
+export default function Bluebusloader() {
+  return (
+    <div className='loader-box'>
+        <div className='loader-back'></div>
+        <div className='loader-blue'>
+            <section className='loader'>
+                <div className='loader-logo'>
+                   
+                </div>
+            </section>
+            <section className='loader1'></section>
+        </div>
+      
+    </div>
+  )
+}
